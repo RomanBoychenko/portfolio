10e2af864d2f6a48a40bf4aa61d85cf5a6a92a02
veronica-ivanova <veronica.ivanova27@gmail.com>
2020-11-09 01:00:49 +0300

    Update
    Contacts with form

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,4 @@
+// FILTER PORTFOLIO
 $(document).ready(function () {
 
     let containerEl = document.querySelector('#projects-container');
@@ -5,6 +6,73 @@ $(document).ready(function () {
         classNames: {
             block: ""
         }
-
 });
 })
+
+//FORM PLACEHOLDER
+const formInputs = document.querySelectorAll('.form-field');
+for(let item of formInputs){
+    const thisPlaceholder = item.nextElementSibling;
+
+    item.addEventListener('focus', function(){
+        thisPlaceholder.classList.add('active');
+    });
+
+    item.addEventListener('blur', function() {
+        if(item.value == ''){
+            thisPlaceholder.classList.remove('active');
+        }
+    });  
+}
+
+
+// FORM VALIDATE
+$('.contacts-form').validate({
+    rules: {
+        email: {
+            required: true,
+            email: true
+        },
+        theme: {
+            required: true
+        },
+        message: {
+            required: true
+        }
+    },
+    messages: {
+        email: {
+            required: 'Введите email',
+            email: 'отсутствует символ @'
+        },
+        theme: {
+            required: 'Введите тему сообщения'
+        },
+        message: {
+            required: 'Введите текст сообщения'
+        }
+    },
+    submitHandler: function (form) {
+        ajaxFormSubmit();
+    }
+})
+
+// function AJAX запроса на сервер
+function ajaxFormSubmit() {
+    let string = $('.contacts-form').serialize(); //сохранение введеных данных
+
+    //Формируем AJAX запрос
+    $.ajax({
+        type: "POST", // тип запроса
+        url: "php/mail.php", //куда отправляем запрос
+        data: string,
+
+        //Функция, если успешно
+        success: function (html) {
+            $('.contacts-form').slideUp(800);
+            $('#answer').html(html);
+        }
+    });
+    //Чтобы по Submit больше ничего не выполнялось - делаем возврат false, чтобы прервать
+    return false;
+}
