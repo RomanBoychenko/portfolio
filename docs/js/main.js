d089a0166419f3af592ef9c80fe2ef2c847bd070
veronica-ivanova <veronica.ivanova27@gmail.com>
2020-11-18 22:18:00 +0300

    Update

--- docs/js/main.js
+++ docs/js/main.js
@@ -36,12 +36,6 @@ $(document).ready(function () {
         overlayEl.classList.remove('active');
         bodyEl.classList.remove('noscroll');
     });
-
-
-
-
-
-
         //NAVE-PAGE
     $('#page-nav').onePageNav({
         currentClass: 'active',
@@ -145,8 +139,24 @@ function ajaxFormSubmit() {
         }
     })
 
-
-
+    // валидация поле телефона
+    // $(".phone").mask("+7(999)999-99-99");
+    // //ф-ция для позиции курсора
+    // $.fn.setCursorPosition = function (pos) {
+    //     if ($(this).get(0).setSelectionRange) {
+    //         $(this).get(0).setSelectionRange(pos, pos);
+    //     } else if ($(this).get(0).createTextRange) {
+    //         var range = $(this).get(0).createTextRange();
+    //         range.collapse(true);
+    //         range.moveEnd('character', pos);
+    //         range.moveStart('character', pos);
+    //         range.select();
+    //     }
+    // };
+    // //ф-ция для позиции курсора для нашего поля phone
+    // $('.phone').click(function () {
+    //     $(this).setCursorPosition(3); // set position number
+    // });
 })
 
     
